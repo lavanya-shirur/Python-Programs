92aa07c8aa78d8c0af012f1f87513399e6a4ec7b
lavanya <lavanya.s.shirur@gmail.com>
2018-06-05 13:39:30 -0500

    data type
    program 1 from hacker rank practice

---
+++ hrdatatype.py
@@ -0,0 +1,28 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+print("enter i2 value")
+i2=int(input())
+#int(i2)
+print("i2 value is",i2)
+print("enter d2 value")
+d2=float(input())
+s2=input()
+#s2='is the best place to learn and practice coding!'
+
+# Print the sum of both integer variables on a new line.
+sumofint=i+i2
+
+# Print the sum of the double variables on a new line.
+sumofdouble=d+d2
+
+# Concatenate and print the String variables on a new line
+sumofstrings=s+s2
+
+# The 's' variable above should be printed first.
+print(sumofint)
+print(sumofdouble)
+print(sumofstrings)
